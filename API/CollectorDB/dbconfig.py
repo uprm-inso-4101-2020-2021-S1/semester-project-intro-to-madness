46ac57a871c1441ba491d29da6ddc723e7805ffb
Luis Rivera <luis.rivera93@upr.edu>
2020-11-22 23:38:59 -0400

    Update dbconfig.py
    added remote database credentials

--- API/CollectorDB/dbconfig.py
+++ API/CollectorDB/dbconfig.py
@@ -3,12 +3,15 @@ pg_config = {
 #     'passwd': 'd@nd@nXD',
 #     'dbname': 'collectorsWiki'
 
-    #'user': 'postgres',
-    #'passwd' : 'Cremora1',
-    #'dbname' : 'appdb'
+#     'user': 'postgres',
+#     'passwd' : 'Cremora1',
+#     'dbname' : 'appdb'
 
-    'user': 'postgres',
+#     'user': 'postgres',
+#     'passwd': 'IntroSoftware',
+#     'dbname': 'CollectorsWiki'
+    
+    'user': 'uqzpjynjyjxmvy',
     'passwd': 'IntroSoftware',
-    'dbname': 'CollectorsWiki'
-
+    'dbname': '851fe84df48120b6747150b24f4d93d2b33f1a28dbde4a25eb725c87b8ff489a' 
 }
