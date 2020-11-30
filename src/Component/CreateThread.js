f4138aaa76e60b5f0975c0da3f7f835a1c20a24d
Diego Cintrón <diego.cintron1@upr.edu>
2020-11-29 20:06:32 -0400

    Minor changes

--- src/Component/CreateThread.js
+++ src/Component/CreateThread.js
@@ -47,7 +47,6 @@ class CreateThread extends Component {
         }
     
         createItem(newItem).then(res => {})
-        
 
         createThread(newThread).then(res => {
           this.props.history.push('/')
@@ -60,89 +59,82 @@ class CreateThread extends Component {
     
           <table style={{backgroundColor: "#2F4F4F", borderStyle: "solid", position: "relative", margin: "auto", top: '60px'} }>
               <tr><th>
-              
-              <form noValidate onSubmit={this.onSubmit}>
-              <h1 className="h3 mb-3 font-weight-normal" style={{marginLeft: "0", padding: "20px 20px"}}>Create Item</h1>
-              <div className="form-group">
-                <label style={{float:"center"}} htmlFor="item_name">Item</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="item_name"
-                  placeholder="Enter the item's name"
-                  value={this.state.item_name}
-                  onChange={this.onChange}
-                />
-              <div className="form-group">
-                <label style={{float:"center"}} htmlFor="average_price">Suggested Price</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="average_price"
-                  placeholder="Enter price"
-                  value={this.state.average_price}
-                  onChange={this.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label style={{float:"center"}} htmlFor="item_description">Description</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="item_description"
-                  placeholder="Describe the item"
-                  value={this.state.item_description}
-                  onChange={this.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label style={{float:"center"}} htmlFor="item_history">History</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="item_history"
-                  placeholder="Enter item's history"
-                  value={this.state.item_history}
-                  onChange={this.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label style={{float:"center"}} htmlFor="image_url">Picture</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="image_url"
-                  placeholder="Insert url"
-                  value={this.state.image_url}
-                  onChange={this.onChange}
-                />
-              </div>
-              <div className="form-group">
-                <label style={{float:"center"}} htmlFor="category">Category</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="category"
-                  placeholder="Item Category"
-                  value={this.state.category}
-                  onChange={this.onChange}
-                />
-              </div>
-              <button
-                type="submit"
-                className="btn btn-lg btn-primary btn-block"
-              >
-                Create!
-              </button>
-              </div>
-            </form>
-                  {/* <div className='bodyItem'><h2>Item Name: </h2></div>
-                  <div className='bodySquare'><img id='picture' src={filler} className="Web-logo" alt="filler" /></div>
-                  <div className='bodySquare'><h3>Price:</h3><p id='input'>$   </p></div>
-                  <div className='bodySquare'><h3>Item Description</h3><p id='input'>   </p></div>
-                  <div className='bodySquare'><h3>Item History</h3><p id='input'>   </p></div>
-                  <div className='bodySquare'><h3>Comments:</h3><p id='input'>No comments</p></div> */}
-                </th></tr>
+                
+                <form noValidate onSubmit={this.onSubmit}>
+                <h1 className="h3 mb-3 font-weight-normal" style={{marginLeft: "0", padding: "20px 20px"}}>Create Item</h1>
+                <div className="form-group">
+                  <label style={{float:"center"}} htmlFor="item_name">Item</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="item_name"
+                    placeholder="Enter the item's name"
+                    value={this.state.item_name}
+                    onChange={this.onChange}/>
+                <div className="form-group">
+                  <label style={{float:"center"}} htmlFor="average_price">Suggested Price</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="average_price"
+                    placeholder="Enter price"
+                    value={this.state.average_price}
+                    onChange={this.onChange}/>
+                </div>
+                <div className="form-group">
+                  <label style={{float:"center"}} htmlFor="item_description">Description</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="item_description"
+                    placeholder="Describe the item"
+                    value={this.state.item_description}
+                    onChange={this.onChange}/>
+                </div>
+                <div className="form-group">
+                  <label style={{float:"center"}} htmlFor="item_history">History</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="item_history"
+                    placeholder="Enter item's history"
+                    value={this.state.item_history}
+                    onChange={this.onChange}/>
+                </div>
+                <div className="form-group">
+                  <label style={{float:"center"}} htmlFor="image_url">Picture</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="image_url"
+                    placeholder="Insert url"
+                    value={this.state.image_url}
+                    onChange={this.onChange}/>
+                </div>
+                <div className="form-group">
+                  <label style={{float:"center"}} htmlFor="category">Category</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="category"
+                    placeholder="Item Category"
+                    value={this.state.category}
+                    onChange={this.onChange}/>
+                </div>
+                <button
+                  type="submit"
+                  className="btn btn-lg btn-primary btn-block">
+                  Create!
+                </button>
+                </div>
+              </form>
+              </th></tr>
+            </table>
+            <table style={{backgroundColor: "#2F4F4F", borderStyle: "solid", position: "relative", margin: "auto", top: '60px'} }>
+              <tr><th>
+                <h2>For the item creation to work, install the following chrome extension.</h2>
+                <a href="https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf">Install me</a>
+              </th></tr>
             </table>
           </div>
         );
